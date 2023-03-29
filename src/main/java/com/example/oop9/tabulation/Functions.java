d280c47cd32e78c2d7d04e9782a93028a2e4225c
DeadMcarb <211142@nuos.edu.ua>
2023-03-29 10:45:57 +0300

    Initial commit

---
+++ src/main/java/com/example/oop9/tabulation/Functions.java
@@ -0,0 +1,2 @@
+package com.example.oop9;public class Functions {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.oop9 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.oop9 to javafx.fxml;
+    exports com.example.oop9;
+}
